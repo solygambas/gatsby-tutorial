234b55cfa76c0531762247baef618f654745435a
solygambas <pietondelair@gmail.com>
2019-10-18 07:27:47 +0200

    add pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
