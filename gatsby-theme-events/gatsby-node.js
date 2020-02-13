a5a5d6aa184046f88201408663a6b8368cc5040d
solygambas <pietondelair@gmail.com>
2020-02-13 14:10:57 +0100

    initial setup, event templates

---
+++ gatsby-theme-events/gatsby-node.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+// Make sure the data directory exists, if not create it
+exports.onPreBootstrap = ({ reporter }) => {
+  const contentPath = "data";
+  if (!fs.existsSync(contentPath)) {
+    reporter.info(`creating the ${contentPath} directory`);
+    fs.mkdirSync(contentPath);
+  }
+};
+// Define the "Event" type
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+      type Event implements Node @dontInfer {
+        id: ID!
+        name: String!
+        location: String!
+        startDate: Date! @dateformat @proxy(from: "start_date")
+        endDate: Date! @dateformat @proxy(from: "end_date")
+        url: String!
+        slug: String!
+      }
+    `);
+};
+// Define resolvers for custom fields
+exports.createResolvers = ({ createResolvers }) => {
+  const basePath = "/";
+  // Quick-and-dirty helper to convert strings into URL-friendly slugs.
+  const slugify = str => {
+    const slug = str
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/(^-|-$)+/g, "");
+    return `/${basePath}/${slug}`.replace(/\/\/+/g, "/");
+  };
+  createResolvers({
+    Event: {
+      slug: {
+        resolve: source => slugify(source.name)
+      }
+    }
+  });
+};
+// query for events and create pages
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const basePath = "/";
+  actions.createPage({
+    path: basePath,
+    component: require.resolve("./src/templates/events.js")
+  });
+  const result = await graphql(`
+    query {
+      allEvent(sort: { fields: startDate, order: ASC }) {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `);
+  if (result.errors) {
+    reporter.panic("error loading events", result.errors);
+    return;
+  }
+  const events = result.data.allEvent.nodes;
+  events.forEach(event => {
+    const slug = event.slug;
+    actions.createPage({
+      path: slug,
+      component: require.resolve("./src/templates/event.js"),
+      context: {
+        eventID: event.id
+      }
+    });
+  });
+};
---
+++ gatsby-theme-events/src/templates/event.js
@@ -0,0 +1,3 @@
+import React from "react";
+const EventTemplate = () => <p>TODO: Build the event page template</p>;
+export default EventTemplate;
